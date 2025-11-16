b855b94eb8063f3d813bb222ee0875e74bc23005
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2025-11-16 17:54:50 +0200

    Update Course.java

--- src/models/Course.java
+++ src/models/Course.java
@@ -3,11 +3,67 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package models;
-
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
- * @author DELL
+ * @author ERR0R
  */
 public class Course {
-    
+    private String title;
+    private String description;
+    private int courseID;
+    private int instructorId;
+    private List<Integer> students = new ArrayList<>(); //IDs
+    private List<Lesson> lessons = new ArrayList<>();
+    //setters
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setCourseID(int courseID){
+        this.courseID=courseID;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public void setInstructorId(int instructorId){
+        this.instructorId=instructorId;
+    }
+    public void setLessons(List<Lesson> lessons){
+        this.lessons=lessons;
+    }
+    public void setStudents(List<Integer> students){
+        this.students=students;
+    }
+   //getters
+    public String getTitle(){
+       return title;
+    }
+    public int getCourseID(){
+        return courseID;
+    }
+    public List<Lesson> getLessons(){
+        return lessons;
+    }
+    public List<Integer> getStudents(){
+        return students;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public int getInstructorId(){
+        return instructorId;
+    }
+    //methods
+    public void addLesson(Lesson lesson){ lessons.add(lesson);}
+    public void removeLesson(int lessonID) {lessons.remove(lessonID);}
+    public void editLesson(int lessonID,Lesson newLesson){lessons.set(lessonID,newLesson);}
+    public void enrollStudent(int studentID) {
+        if (!students.contains(studentID))
+            students.add(studentID);
+    }
+    public void unenrollStudent(int studentID) {
+        students.remove(studentID);
+    }
 }
+
