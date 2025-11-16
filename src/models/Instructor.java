aaa922743335ddd425bf7f3e4a88f663413732d3
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2025-11-16 17:26:30 +0200

    Update Instructor.java

--- src/models/Instructor.java
+++ src/models/Instructor.java
@@ -4,10 +4,30 @@
  */
 package models;
 
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
- * @author DELL
+ * @author ERR0R
  */
-public class Instructor {
-    
+public class Instructor extends User{
+     private List<Integer> createdCourses = new ArrayList<>();
+    //setters and getters
+    public List<Integer> getCreatedCourses(){
+        return createdCourses;
+    }
+    public void setCreatedCourses(List<Integer> createdCourses){
+        this.createdCourses=createdCourses;
+    }
+    //methods
+    public void addCourse(int courseID){
+         if (!createdCourses.contains(courseID)){
+            createdCourses.add(courseID);
+        }
+    }
+    public void removeCourse(int courseID){
+         if (createdCourses.contains(courseID)) {
+            createdCourses.remove(courseID);
+        }
+    }
 }
