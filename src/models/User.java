e1399d117cd81fcdcb2e8e0ed47946d2cc9060e1
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2025-11-16 16:36:48 +0200

    Update User.java

--- src/models/User.java
+++ src/models/User.java
@@ -6,8 +6,21 @@ package models;
 
 /**
  *
- * @author DELL
+ * @author ERR0R
  */
 public class User {
-    
+    private int userID;
+    private String name;
+    private String passwordHash;
+    private String email;
+    //getters
+    public String getName(){ return name;}
+    public String getEmail(){ return email;}
+    public String getPasswordHash(){return passwordHash;}
+    public int getUserID(){ return userID;}
+    //setters
+    public void setName(String name){ this.name=name;}
+    public void setEmail(String email){ this.email=email;}
+    public void setPasswordHash(String passwordHash){ this.passwordHash=passwordHash;}
+    public void setUserID(int userID){ this.userID=userID;}
 }
