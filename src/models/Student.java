896c4bffebd680216e1034f9f1abea7ba3677f72
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2025-11-16 17:01:40 +0200

    Update Student.java

--- src/models/Student.java
+++ src/models/Student.java
@@ -3,11 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package models;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
- * @author DELL
+ * @author ERR0R
  */
-public class Student {
-    
+public class Student extends User {
+    private List<Integer> enrolledCourses = new ArrayList<>();
+    private List<Integer> completedLessons = new ArrayList<>(); //Track progress
+    //setters and getters
+    public List<Integer> getEnrolledCourses(){
+        return enrolledCourses;
+    }
+    public List<Integer> getCompletedLessons(){
+        return completedLessons;
+    }
+    public void setEnrolledCourses(List<Integer> enrolledCourses){
+        this.enrolledCourses=enrolledCourses;
+    }
+    public void setCompletedLessons(List<Integer> completedLessons){
+        this.completedLessons=completedLessons;
+    }
+    //methods
+    public void enrollCourse(int courseId){
+    if (!enrolledCourses.contains(courseId)){
+        enrolledCourses.add(courseId);
+        }
+    }
+    public void completeLesson(int lessonId) {
+     if (!completedLessons.contains(lessonId)) {
+        completedLessons.add(lessonId);
+    }
+}   
 }
